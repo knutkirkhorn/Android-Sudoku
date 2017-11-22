b802191a8589a0520eb7ea83da4a1cd2b3fa4048
Knut Kirkhorn <knutkirk@hotmail.com>
2017-11-22 10:33:53 +0100

    Added more to table

--- app/src/main/java/me/kirkhorn/knut/android_sudoku/NewBoardActivity.java
+++ app/src/main/java/me/kirkhorn/knut/android_sudoku/NewBoardActivity.java
@@ -1,10 +1,11 @@
 package me.kirkhorn.knut.android_sudoku;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import me.kirkhorn.knut.android_sudoku.fragments.CellGroupFragment;
 
@@ -29,8 +30,25 @@ public class NewBoardActivity extends AppCompatActivity implements CellGroupFrag
         startActivity(intent);
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void onGroupClicked(View view) {
+        System.out.println("CLICKED group: " + view.getTag());
+    }
 
+    public void onCellClicked(View view) {
+ //       System.out.println("THIS WORKED");
+        //TableRow tableRow = new TableRow(this);
+//        System.out.println(view.getTag());
+        TextView textView = (TextView) view;
+        textView.setText("2");
+        System.out.println(view.getRootView().getTag());
+        System.out.println(view.getRootView().getTag());
+        System.out.println(view.getRootView().getTag());
+        System.out.println(view.getRootView());
+        System.out.println(view.getRootView().getParent());
+    }
+
+    @Override
+    public void onFragmentInteraction(String string) {
+        System.out.println("CLICKED FRAGMENT");
     }
 }
--- app/src/main/java/me/kirkhorn/knut/android_sudoku/fragments/CellGroupFragment.java
+++ app/src/main/java/me/kirkhorn/knut/android_sudoku/fragments/CellGroupFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import me.kirkhorn.knut.android_sudoku.R;
 
-
+/*
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
@@ -22,12 +24,12 @@ import me.kirkhorn.knut.android_sudoku.R;
 public class CellGroupFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //private String mParam1;
+    //private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,6 +37,7 @@ public class CellGroupFragment extends Fragment {
         // Required empty public constructor
     }
 
+    /*
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -44,21 +47,21 @@ public class CellGroupFragment extends Fragment {
      * @return A new instance of fragment CellGroupFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CellGroupFragment newInstance(String param1, String param2) {
+    /*public static CellGroupFragment newInstance(String param1, String param2) {
         CellGroupFragment fragment = new CellGroupFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -66,14 +69,24 @@ public class CellGroupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cell_group, container, false);
+        View view = inflater.inflate(R.layout.fragment_cell_group, container, false);
+
+        //TextView textView =
+        //TableRow tableRow = new TableRow(this);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
+    /*public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onFragmentInteraction();
         }
+    }*/
+
+    public void onClickTest(View view) {
+        System.out.println("THIS IS A TEST");
+        mListener.onFragmentInteraction("asd");
     }
 
     @Override
@@ -105,6 +118,6 @@ public class CellGroupFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction(String string);
     }
 }
