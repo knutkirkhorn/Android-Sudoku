cc6e4c90a200d7b1355225259bd7b2d9cf1ad7e2
Knut Kirkhorn <knutkirk@hotmail.com>
2017-11-22 08:44:48 +0100

    Added Sudoku table
    Added Sudoku table and fragments for the table

--- app/src/main/java/me/kirkhorn/knut/android_sudoku/NewBoardActivity.java
+++ app/src/main/java/me/kirkhorn/knut/android_sudoku/NewBoardActivity.java
@@ -1,17 +1,36 @@
 package me.kirkhorn.knut.android_sudoku;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import me.kirkhorn.knut.android_sudoku.fragments.CellGroupFragment;
 
 /**
  * Created by Knut on 19.11.2017.
  */
 
-public class NewBoardActivity extends AppCompatActivity {
+public class NewBoardActivity extends AppCompatActivity implements CellGroupFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_board);
+    }
+
+    public void onGoBackButtonClicked(View view) {
+        finish();
+    }
+
+    public void onShowInstructionsButtonClicked(View view) {
+        Intent intent = new Intent("me.kirkhorn.knut.InstructionsActivity");
+        startActivity(intent);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
